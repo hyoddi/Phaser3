61f17a6330fdca72473c10f0fdc7d19fdd2ebb35
hyoddi <110017938+hyoddi@users.noreply.github.com>
2024-11-28 14:46:55 +0000

    first

---
+++ breaker-exgame/breakout.js
@@ -0,0 +1,37 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+let canvas_width = canvas.width;
+let canvas_height = canvas.height;
+
+let ball_x = canvas.width / 2;
+let ball_y = canvas.height - 60;
+let ball_radiaus = 10;
+let ball_dx = 4;
+let ball_dy = -4;
+
+function drawBall(){
+    ctx.beginPath(); // 새로운 경로 시작
+    ctx.arc(ball_x, ball_y, ball_radiaus, 0, Math.PI * 2); // 원 그리기
+    ctx.fillStyle = "#FFFFFF"; // 공 색깔 (하양)
+    ctx.fill(); // 원 채우기
+    ctx.closePath(); // 경로 닫기
+}
+
+// 애니메이션 프레임마다 실행되는 함수
+function draw(){ 
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // 이전 프레임 지우고, 새로운 프레임 그릴 준비
+    drawBall(); // 공 그리기
+
+    if(ball_x < ball_radiaus || ball_x > canvas_width - ball_radiaus) ball_dx = -ball_dx;
+    if(ball_y < ball_radiaus || ball_y > canvas_height - ball_radiaus) ball_dy = -ball_dy;
+
+    // 공 위치 업데이트
+    ball_x += ball_dx;
+    ball_y += ball_dy;
+
+
+    requestAnimationFrame(draw); // 다음 프레임 요청
+}
+
+draw();
